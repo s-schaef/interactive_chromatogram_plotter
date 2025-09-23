a24599d7bd995c866c5f6866bca10f0512c0f834
s-schaef <89992887+s-schaef@users.noreply.github.com>
2025-09-23 17:13:35 +0200

    allow data get from tab

--- chromatogram_plotter.py
+++ chromatogram_plotter.py
@@ -189,7 +189,7 @@ with tab1:
     custom_names = {}
     if st.session_state.data_dict:
         st.header("Custom Sample Names")
-        for filename in st.session_state.data_dic.keys():
+        for filename in st.session_state.data_dict.keys():
             st.settion_state.custom_names[filename] = st.text_input(
                 f"Custom name for {filename}",
                 value=default_names[filename],
