ef663cd1ab9850f21721f887c54a3e2b719aa77d
s-schaef <89992887+s-schaef@users.noreply.github.com>
2025-09-29 12:09:06 +0200

    fix individual axlabels

--- chromatogram_plotter.py
+++ chromatogram_plotter.py
@@ -16,11 +16,11 @@ def generate_plots(data_dict, custom_names, x_data_dict, plot_configs, external_
     
     # Handle subplot layout
     if len(valid_configs) in [1, 2, 3]:
-        fig, axs = plt.subplots(1, len(valid_configs), figsize=(5*len(valid_configs), 5), squeeze=False, sharey=False, sharex=False)
+        fig, axs = plt.subplots(1, len(valid_configs), figsize=(5*len(valid_configs), 5), squeeze=False)
     elif len(valid_configs) == 4:
-        fig, axs = plt.subplots(2, 2, figsize=(10, 10), squeeze=False, sharey=False, sharex=False)
+        fig, axs = plt.subplots(2, 2, figsize=(10, 10), squeeze=False)
     else:
-        fig, axs = plt.subplots(math.ceil(len(valid_configs)/3), 3, figsize=(15, 10), squeeze=True, sharey=False, sharex=False)
+        fig, axs = plt.subplots(math.ceil(len(valid_configs)/3), 3, figsize=(15, 10), squeeze=True)
     axs = axs.flat
 
     # remove unused axis in case of 5 plots
@@ -31,13 +31,9 @@ def generate_plots(data_dict, custom_names, x_data_dict, plot_configs, external_
         ax = axs[i]
         
         if external_label and custom_legend:
-            #color_iter = cycle(color_cycle)
-            
             for filename in config['files']:
-                # if j >= len(labels):
-                #     break  # only plot as many lines as labels
                 if filename in data_dict:
-                    ax.plot(x_data_dict[filename], data_dict[filename])#, color=next(color_iter))
+                    ax.plot(x_data_dict[filename], data_dict[filename])
         else:
             for filename in config['files']:
                 if filename in data_dict:
@@ -48,23 +44,44 @@ def generate_plots(data_dict, custom_names, x_data_dict, plot_configs, external_
                     )
             ax.legend(loc="upper left", fontsize='small')
         ax.set_title(config['title'])
+    
     if suptitle_enabled:
         fig.suptitle(suptitle, fontsize=16)
-        
+    
     if supaxes_enabled:
         fig.supxlabel("Time (min)")
         fig.supylabel("Intensity (mAU)")
+        
+        # Determine the layout shape
+        if len(valid_configs) in [1, 2, 3]:
+            rows, cols = 1, len(valid_configs)
+        elif len(valid_configs) == 4:
+            rows, cols = 2, 2
+        else:
+            rows, cols = math.ceil(len(valid_configs)/3), 3
+        
+        # Hide tick labels for non-edge axes
+        for i, ax in enumerate(axs):
+            if not ax.get_visible():
+                continue
+                
+            # Calculate row and column position
+            row = i // cols
+            col = i % cols
+            
+            # Only show y-tick labels for leftmost axes
+            if col > 0:
+                ax.tick_params(axis='y', labelleft=False)
+            
+            # Only show x-tick labels for bottom axes
+            if row < (rows - 1):
+                ax.tick_params(axis='x', labelbottom=False)
     else:
         all_ylims = []
         for ax in axs:
             if ax.get_visible():
                 ax.set_xlabel("Time (min)")
                 ax.set_ylabel("Intensity (mAU)")
-                
-                # Plot the data for this axis
-                for filename in ax.get_lines():
-                    if filename.get_label() in data_dict:
-                        ax.plot(x_data_dict[filename.get_label()], data_dict[filename.get_label()])
                 
                 # Update limits based on the plotted data
                 ax.relim()
@@ -79,7 +96,6 @@ def generate_plots(data_dict, custom_names, x_data_dict, plot_configs, external_
             for ax in axs:
                 if ax.get_visible():
                     ax.set_ylim(y_min, y_max)
-
     
     if external_label and custom_legend:
         labels = [line.strip() for line in custom_legend.splitlines() if line.strip()]
